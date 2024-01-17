c64a9f477e58d219df554bef675ba91658926248
Matheus Felipe <matheusibnf@hotmail.com>
2024-01-16 21:51:45 -0300

    creating arrays

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ Capture 10 items para compor a lista de um supermercado.
 Após capturar os 10 items, imprima-os, separando por vírgula.
 */
 
+let items = [];
 
 for (let item = 0; item < 10; item++) {
-  prompt("Digite o item " + (item + 1 + ":") )
+  items = prompt("Digite o item " + (item + 1 + ":"))
 }
+
 
