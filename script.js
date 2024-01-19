2ec06641c911864b6d27771b188b8b5e21e037e3
Matheus Felipe <matheusibnf@hotmail.com>
2024-01-18 22:52:46 -0300

    add interpolation in the alert

--- script.js
+++ script.js
@@ -19,8 +19,11 @@
 let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
 const randomNumber = Math.round(Math.random() * 10)
 
+let xAttempts = 1
+
 while (Number(result) != randomNumber) {
   result = prompt("Erro, tente novamente:")
+  xAttempts++;
 }
 
-alert("Parabéns! Você advinhou o número em x tentativas")
+alert(`Parabéns! Você advinhou o número em ${xAttempts} tentativas`)
