1a6a2d2caefa43146c7dc80a74c9c0af1dd6aba7
Matheus Felipe <matheusibnf@hotmail.com>
2024-01-18 22:06:35 -0300

    add studyng about while

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+  ** Jogo da advinhação **
+
+  Apresente a mensagem ao usuário:
+  "Advinhe o número que estou pensando? Está entre 0 e 10"
+
+  Crie um lógica para gerar um número aleatório 
+  e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+
+  Enquanto o usuário não adivinhar o número, mostrar a mensagem:
+  "Erro, tente novamente:"
+
+  Caso o usuário acerte o número, apresentar a mensagem:
+  "Parabéns! Você advinhou o número em x tentativas"
+
+  Substitua o "x" da mensagem, pelo número de tentativas
+*/
+
+const result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+const match = Number(result) == randomNumber
+
+while (true) {
+  console.log("alo")
+} 
