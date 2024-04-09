cb0c936f813ceb2f878cb2915afa9a2094c3ba3f
Matheus Felipe <matheusibnf@hotmail.com>
2024-04-08 22:20:35 -0300

    add making exercise about function again

--- script.js
+++ script.js
@@ -17,10 +17,45 @@ const patients = [
     weight: 78,
     height: 182,
   },
+  {
+    name: "Matheus",
+    age: 24,
+    weight: 79,
+    height: 183,
+  },
+  {
+    name: "Camila",
+    age: 29,
+    weight: 64,
+    height: 169,
+  },
 ];
+// ${(patients.weight / (patients.height / 100) ** 2).toFixed(2)}
 
-alert(`
-  Paciente ${patients[0].name} possui o IMC de
-  ${patients[0].weight / (patients[0].height / 100) ** 2} 
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2);
+}
 
-`);
+function printPatientIMC(patient) {
+  return `
+    Paciente ${patient.name} possui o IMC de
+    ${IMC(patient.weight, patient.height)}
+  
+  `;
+}
+
+for (let patient of patients) {
+  let IMCmessage = printPatientIMC(patient);
+  alert(IMCmessage);
+}
+
+//criação, depois o javascript irá guardar em memória
+// function liquidificador(fruta1, fruta2) {
+//   const suco = fruta1 + fruta2;
+//   return suco;
+// }
+
+// //Execução
+// let MeuSuco = liquidificador("maça" ,  "banana")
+
+// alert(MeuSuco)
