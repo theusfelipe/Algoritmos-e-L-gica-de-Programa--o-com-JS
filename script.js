05a41f19acadb9acd3419fb74f77e8620317674a
Matheus Felipe <matheusibnf@hotmail.com>
2024-04-08 21:15:39 -0300

    add backing to study js again

--- script.js
+++ script.js
@@ -1,63 +1,26 @@
-/*Faça um programa que tenha um menu e apresente a seguinte mensagem:
-
-  Olá usuário! Digite o número da opção desejada
-
-    1. Cadastrar um item na lista
-    2. Mostrar itens cadastrados
-    3. Sair do programa
-
-  ---
-  O programa deverá capturar o número digitado pelo usuário e mostrar o seguintes cenários:
-
-  Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
-  Caso o usuário digite 2, ele poderá ver os itens cadastrados
-    Se não houver nenhum item cadastrado, mostrar a mensagem:
-      "Não existem itens cadastrados"
-  Caso o usuário digite 3, a aplicação deverá ser encerrada.
-
-*/
-
 /*
-  Dados de entrada do usuário
-  1- Número desejado
-  2- item da lista
+Dada uma lista de pacientes, descubra o IMC  de cada paciente e imprima
 
-  Variáveis
-  1- Opção digitada
-  2- Lista de itens
+"Paciente X possui o IMC de: Y"
+
+Onde X é o nome do paciente e Y é o IMC desse paciente
+
+Crie uma função para fazer o cálculo de IMC
 */
 
-let option
-let items = []
+//    Peso / (altura * altura) */
 
-while (option != 3) {
-  option = Number(
-    prompt(`
-  Olá usuário! Digite o número da opção desejada
-  
-  1. Cadastrar um item na lista
-  2. Mostrar itens cadastrados
-  3. Sair do programa
-  
-  `)
-  )
+const patients = [
+  {
+    name: "Luiz",
+    age: 24,
+    weight: 78,
+    height: 182,
+  },
+];
 
-  switch (option) {
-    case 1:
-      let item = prompt("Digite o nome do item")
-      items.push(item)
-      break
-    case 2:
-      if (items.length === 0) {
-        alert("Não existem itens cadastrados!")
-      } else {
-        alert(items)
-      }
-      break
-    case 3:
-      alert("Tchauzinho!")
-      break
-    default:
-      alert("Opção inválida, Tente novamente!")
-  }
-}
+alert(`
+  Paciente ${patients[0].name} possui o IMC de
+  ${patients[0].weight / (patients[0].height / 100) ** 2} 
+
+`);
