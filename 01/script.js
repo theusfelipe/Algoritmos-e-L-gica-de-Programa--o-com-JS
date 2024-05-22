047dfeabd9f95fd49626d3c2383150b84fc79f6d
Matheus Felipe Dos Santos <105308149+theusfelipe@users.noreply.github.com>
2024-05-22 17:24:59 +0000

    making the exercise of javascript

--- 01/script.js
+++
@@ -1,8 +0,0 @@
-let nome = prompt("Olá, Boa noite! Qual é o seu nome?")
-alert("Olá! " + nome + "!!")
-let question = prompt("Como posso ajudar você esta noite?")
-alert("Ok aguarde um minuto!, irei solucionar seu problema")
-let send = prompt("Diga a ela, que a ama, e que está morrendo de Saudade, tenho certeza que ela está também!")
-alert("Sei que ela irá amar, pois foram feitos um para o outro!")
-let otherAsk = prompt("Mais alguma coisa que você precisa?")
-alert("Então ok, termino meu atendimento aqui, tenha uma boa noite! ")
---
+++ Hello World/script.js
@@ -0,0 +1,3 @@
+let world = "Hello World!";
+
+alert(let)
