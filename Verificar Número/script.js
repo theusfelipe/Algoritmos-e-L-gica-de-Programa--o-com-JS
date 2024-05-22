7b0d76356487e6035a3eed881662502a84210970
Matheus Felipe Dos Santos <105308149+theusfelipe@users.noreply.github.com>
2024-05-22 18:01:16 +0000

    veficiarnumero

---
+++ Verificar Número/script.js
@@ -0,0 +1,8 @@
+/*
+Crie um script que declare uma variável e verifique se o seu valor é número. 
+Se for, exiba a mensagem  "É um número" caso contrário, exiba a mensagem 
+"não é um número"  Exemplo: Typeof 
+*/
+
+
+
