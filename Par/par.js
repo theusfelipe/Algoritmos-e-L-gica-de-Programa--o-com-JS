b1ae3502471793ab7d8f18a72140eb1ccd1282da
Matheus Felipe Dos Santos <105308149+theusfelipe@users.noreply.github.com>
2024-05-28 17:30:30 +0000

    pair

---
+++ Par/par.js
@@ -0,0 +1,23 @@
+let minhaVariavelPar = 10;
+
+// Função para verificar se um valor é um número
+function isNumber (value) {
+    return typeof value === 'number';
+} 
+
+// Função para verificar se um número é par
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+
+// Verificação do tipo e se é par
+if (isNumber(minhaVariavel)) {
+    if (isEven(minhaVariavel)) {
+        console.log("É um número par");
+    } else {
+        console.log("Não é um número par");
+    }
+} else {
+    console.log("Não é um número");
+}
